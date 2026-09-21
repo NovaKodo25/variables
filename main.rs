b17423839c667103a4bc155dd2684c4e39659dc2
NovaKodo <NovaKodo@proton.me>
2026-09-21 22:06:12 +0400

    Add files via upload

--- main.rs
+++ main.rs
@@ -8,6 +8,7 @@ fn main() {
 }
 */
 
+/*
 fn main() {
     let x = 5;
 
@@ -19,4 +20,44 @@ fn main() {
     }
 
     println!("The value of x is: {}", x);
+
+} 
+*/
+
+/*
+fn main() {
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {y}");
+}
+*/
+
+fn main() {
+
+    // integer
+    let num: i8 = 50;
+    println!("First value: {}", num);
+
+    let num: i8 = 45;
+    println!("Second value: {}", num);
+
+    // let num = "hello";
+    // println!("Third value: {}", num);
+
+    let num: i8 = num + 10;
+    println!("Third value: {}", num);
+
+    // float
+    let num: f32 = 10.5;
+    println!("Fourth value: {}", num);
+
+    // boolean
+    let is_true: bool = true;
+    println!("Fifth value: {}", is_true);
+
+    // character
+    let letter: char = '#';
+    println!("Sixth value: {}", letter);
 }
